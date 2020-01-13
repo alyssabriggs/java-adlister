268328a0d12e98c7761c7802b6601631fcc5c1e0
alyssalanae <briggs.l.alyssa@gmail.com>
2020-01-13 14:29:39 -0600

    Completed sessions exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,12 +3,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        boolean isUser = false;
+
+        if (session.getAttribute("user") != null)
+            isUser = (boolean) session.getAttribute("user");
+
+        if (isUser)
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        else
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -17,6 +29,8 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("user", true);
+            request.getSession().setAttribute("username", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,21 +6,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
 
-        boolean isUser = false;
-
         if (session.getAttribute("user") != null)
-            isUser = (boolean) session.getAttribute("user");
+            session.setAttribute("user", false);
 
-        if (isUser) {
-            request.setAttribute("username", session.getAttribute("username"));
-            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        } else
-            request.getRequestDispatcher("/login").forward(request, response);
+        request.getRequestDispatcher("/login").forward(request, response);
     }
 }
