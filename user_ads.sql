37c7717d393e4978f559146cc005ab9e1fea7095
alyssalanae <briggs.l.alyssa@gmail.com>
2020-01-14 13:44:41 -0600

    Added files

---
+++ user_ads.sql
@@ -0,0 +1,20 @@
+use adlister_db;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR (20),
+    email VARCHAR (50),
+    password VARCHAR (15),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS ads;
+CREATE TABLE IF NOT EXISTS ads (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    title VARCHAR (50),
+    description VARCHAR (100),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
